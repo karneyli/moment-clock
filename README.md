b2a89bf4c1ef2fa352f45cc5b08d8020eb4cc732
Karney Li <karneyli@gmail.com>
2015-05-22 08:23:21 -0400

    documentation

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # moment-clock [![Build Status](https://travis-ci.org/karneyli/moment-clock.svg?branch=master)](https://travis-ci.org/karneyli/moment-clock)
 
-Simple factory for moment(), that allows setting the time to something static.
+Simple factory for moment(), that allows setting the time to something static.  
 
-## Links
+Often times we would like to test our code with a specific time in mind to verify that the behavior is correct when the time is set forward, backwards, or kept the same.  This can be often challenging if you're using moment's constructors straight up because there's no way to tell moment what the current time is and for time to not march forward.
 
+## Installation
+
+    npm install moment-clock --save
+
+## Usage
+
+Require moment-clock whenever you need to get the current time instead of using the moment() constructor.  
+
+    var clock = require('moment-clock');
+    var now = clock.getTime();
+
+If in your tests you want to set the time that is returned by moment-clock:
+		
+		var clock = require('moment-clock');
+		var moment = require('moment');
+
+		clock.setTime(moment('2014-05-02'));
+
+		var now = clock.getTime();  
+		// now is 2014-05-02, no matter where #getTime is called from it will return 2014-05-02 
+
+To reset the clock to the actual time:
+
+		clock.reset();
+
+## License
+
+Released under the terms of the [MIT License](https://github.com/karneyli/moment-clock/blob/master/LICENSE-MIT)
--- test/moment-clock-test.js
+++ test/moment-clock-test.js
@@ -1,44 +1,44 @@
-var Clock = require('../');
+var clock = require('../');
 
 describe("moment-clock", function() {
 
 
   beforeEach(function() {
-    Clock.reset();
-    assert.isUndefined(Clock.now);
+    clock.reset();
+    assert.isUndefined(clock.now);
   });
 
   it("#setTime then #getTime", function() {
-    Clock.setTime(moment('2015-05-20'));
-    assert(moment('2015-05-20').isSame(Clock.getTime()));
-    assert(moment('2015-05-20').isSame(Clock.getTime()));
+    clock.setTime(moment('2015-05-20'));
+    assert(moment('2015-05-20').isSame(clock.getTime()));
+    assert(moment('2015-05-20').isSame(clock.getTime()));
   });
 
   it("#setTime then #reset", function() {
-    Clock.setTime(moment('2015-05-20'));
-    assert(moment('2015-05-20').isSame(Clock.getTime()));
+    clock.setTime(moment('2015-05-20'));
+    assert(moment('2015-05-20').isSame(clock.getTime()));
 
-    assert.isDefined(Clock.now);
-    Clock.reset();
-    assert.isUndefined(Clock.now);
+    assert.isDefined(clock.now);
+    clock.reset();
+    assert.isUndefined(clock.now);
 
-    assert(moment('2015-05-20').isBefore(Clock.getTime()));
-    assert.isUndefined(Clock.now);
+    assert(moment('2015-05-20').isBefore(clock.getTime()));
+    assert.isUndefined(clock.now);
   });
 
   it("all clocks tell same time", function() {
-    var AnotherClock = require('../');
+    var anotherClock = require('../');
     var now = moment('2015-05-20');
-    Clock.setTime(now);
+    clock.setTime(now);
 
-    assert(Clock.getTime().isSame(now));
-    assert(AnotherClock.getTime().isSame(now));
-    assert.notStrictEqual(Clock.getTime(), AnotherClock.getTime());
+    assert(clock.getTime().isSame(now));
+    assert(anotherClock.getTime().isSame(now));
+    assert.notStrictEqual(clock.getTime(), anotherClock.getTime());
 
     var yesterday = moment('2015-05-19');
-    Clock.setTime(yesterday);
-    assert(Clock.getTime().isSame(yesterday));
-    assert(AnotherClock.getTime().isSame(yesterday));
+    clock.setTime(yesterday);
+    assert(clock.getTime().isSame(yesterday));
+    assert(anotherClock.getTime().isSame(yesterday));
 
   }); 
 
